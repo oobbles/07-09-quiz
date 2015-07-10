b9153a489408ccd3068908d0dede6cbc2bac25cd
Lisa Davis <oobbles@gmail.com>
2015-07-10 15:10:16 -0500

    using db to get correct answers

--- public/global.js
+++ public/global.js
@@ -1,23 +1,17 @@
 var question = document.getElementsByClassName("question");
-
 var total_right = 0;
-
 var current_question = 0;
-
 var question_result = document.getElementById("question_result");
-var correct_answers =[1, 1, 1]
-// var question = document.getElementById("question");
-// question.innerText = questions[current_question].question;
 
-// var choices = document.getElementById("choices");
-// choices.innerText=questions[current_question].choice1 + ", " + questions[current_question].choice2 + ", " + questions[current_question].choice3;
 
-var answer = document.getElementById("answer");
 var given_answer = function(){
+  var answer = document.getElementById("a" +(current_question +1));
   return answer.value;
 }
-var is_correct_answer = function(answer_text){
-  if (answer_text==correct_answers[current_question]){
+
+var is_correct_answer = function(answer_text,correct_answer){
+  if (answer_text==correct_answer)
+    {
   total_right++;
   return true;
   }
@@ -25,12 +19,11 @@ var is_correct_answer = function(answer_text){
     return false;
   }
 }
+
 var next_question = function(){
   if(current_question == question.length-1){
     question_result.innerHTML="<h6>Final Right: " + total_right+"</h6>";
-
-  }
-
+    }
   else{
     var current_question_id= current_question +1;
     var next_question_id = current_question_id + 1;
@@ -41,8 +34,6 @@ var next_question = function(){
     }
   }
 
-
-
 var update_question_result = function(correct){
   if (correct==true){
     question_result.innerText = "Success!";
@@ -52,9 +43,15 @@ var update_question_result = function(correct){
   }
 }
 
+var right_answer = function(){
+  var selecter = document.getElementById("a" +(current_question +1));
+  var thenumber = selecter.getAttribute('data-correct');
+ return thenumber;
+}
 var process_answer_submission = function(){
   var user_answer = given_answer();
-  update_question_result(is_correct_answer(user_answer));
+  var good_answer = right_answer();
+  update_question_result(is_correct_answer(user_answer, good_answer));
 }
 // element.addEventListener(‘click’, handler);
 
